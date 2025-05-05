87b783f71b099c5524345d5651e4a0817457faeb
Zeeshan Siddiqui <zeeshansd767@gmail.com>
2025-05-05 20:09:02 +0500

    commit changes

--- server/app.js
+++ server/app.js
@@ -22,7 +22,10 @@ app.get('/login', (req, res) => {
 app.post('/register', async (req, res) => {
     let { username, name, email, age, password } = req.body
     let user = await userModel.findOne({ email })
-    if (user) return res.status(500).send('User Already Resgistered')
+    if (user) {
+        return res.status(500).send(`User Already Resgistered`)
+
+    }
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(password, salt, async (err, hash) => {
             let user = await userModel.create({
@@ -60,12 +63,43 @@ const isloggedIn = (req, res, next) => {
         req.user = data
         next()
     }
-
 }
 
 app.get('/profile', isloggedIn, async (req, res) => {
-    let user = await userModel.findOne({ email: req.user.email })
-    res.render('profile', {user})
+    let user = await userModel.findOne({ email: req.user.email }).populate('posts')
+    res.render('profile', { user })
 })
 
-app.listen(3000)
+app.post('/post', isloggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email })
+    let post = await postModel.create({
+        user: user._id,
+        content: req.body.content,
+    })
+    user.posts.push(post._id)
+    await user.save()
+    res.redirect('/profile')
+})
+
+app.get('/like/:id', isloggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate('user')
+    if (post.likes.indexOf(req.user.userid)===-1) {
+        post.likes.push(req.user.userid)
+    }else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1)  
+    }
+    await post.save()
+    res.redirect('/profile')
+})
+
+app.post('/update/:id', isloggedIn, async (req, res) => {
+    let post = await postModel.findOneAndUpdate({ _id: req.params.id }, {content: req.body.content})
+    res.redirect('/profile')
+})
+
+app.get('/edit/:id', isloggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate('user')
+    res.render('edit', {post})
+})
+
+app.listen(3000);
